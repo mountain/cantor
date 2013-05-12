56efdf3d045ce2b926def0e8e58ca3b82bfdbbec
Mingli Yuan <mingli.yuan@gmail.com>
2013-05-12 20:19:23 +0800

    refine interface and add some impl in Relation

--- src/main/scala/math/Relation.scala
+++ src/main/scala/math/Relation.scala
@@ -1,81 +1,129 @@
 package math
 
-/**
- * Relation
- *
- * A relation under some representation in T
- *
- */
+/** Relation
+  *
+  * A relation under some representation in T
+  *
+  */
 
-trait Relation[T, A <: Set[T], B <: Set[T]] {
-  
-  def domian(): A
-  def codomain(): B
+trait Relation[T] {
 
-  def holds(a: T, b: T): Boolean
-  
-  def includes(another: Relation[T, A, B]): Boolean
+  def domian() : Set[T]
+  def codomain() : Set[T]
 
-  def conjunct(another: Relation[T, A, B]): Relation[T, A, B]
-  def disjunct(another: Relation[T, A, B]): Relation[T, A, B]
-  def complement(): Relation[T, A, B]
+  def empty() : Relation[T]
+  def complete() : Relation[T]
 
-  def inverse(): Relation[T, B, A]
+  def id() : Relation[T]
+  def lid() : Relation[T]
+  def rid() : Relation[T]
 
-  def composite[C <: Set[T]](another: Relation[T, B, C]): Relation[T, A, C]
+  def holds( a : T, b : T ) : Boolean
+
+  def includes( another : Relation[T] ) : Boolean
+
+  def conjunct( another : Relation[T] ) : Relation[T]
+  def disjunct( another : Relation[T] ) : Relation[T]
+  def complement() : Relation[T]
+
+  def inverse() : Relation[T]
+
+  def composite[C <: Set[T]]( another : Relation[T] ) : Relation[T]
+
+  def functional() : Boolean = {
+    this.rid.includes( this.inverse.composite( this ) )
+  }
+
+  def total() : Boolean = {
+    this.inverse.composite( this ).includes( this.rid )
+  }
+
+  def injective() : Boolean = {
+    this.lid.includes( this.composite( this.inverse ) )
+  }
+
+  def surjective() : Boolean = {
+    this.composite( this.inverse ).includes( this.lid )
+  }
+
+  def reflexive() : Boolean = {
+    this.includes( this.id )
+  }
+
+  def transitive() : Boolean = {
+    this.includes( this.composite( this ) )
+  }
 
 }
 
-case class DefinableRelation[T, A <: Set[T], B <: Set[T]](val dom: A, val codom: B, val definition: (T, T) => Boolean)
-  extends Relation[T, A, B] {
+case class DefinableRelation[T, A <: Set[T], B <: Set[T]]( val dom : A, val codom : B, val definition : ( T, T ) => Boolean )
+    extends Relation[T] {
 
-  def domian(): A = dom
-  def codomain(): B = codom
- 
-  def holds(a: T, b: T): Boolean = definition(a, b)
-  
-  def includes(another: Relation[T, A, B]): Boolean = 
+  def domian() : A = dom
+  def codomain() : B = codom
+
+  def holds( a : T, b : T ) : Boolean = definition( a, b )
+
+  def includes( another : Relation[T] ) : Boolean =
     another match {
-      case DefinableRelation(anotherDom, anotherCodom, anotherDefinition) =>
-      anotherCodom.forall((anotherTarget) => anotherDom.forall((anotherSource) => definition(anotherSource, anotherTarget)))
+      case DefinableRelation( anotherDom, anotherCodom, anotherDefinition ) =>
+        anotherCodom.forall( ( anotherTarget ) => anotherDom.forall( ( anotherSource ) => definition( anotherSource, anotherTarget ) ) )
       case _ => ???
     }
 
-  def conjunct(another: Relation[T, A, B]): Relation[T, A, B] = {
+  def conjunct( another : Relation[T] ) : Relation[T] = {
     another match {
-      case DefinableRelation(anotherDom, anotherCodom, anotherDefinition) =>
-        new DefinableRelation(dom, codom, (x, y) => this.definition(x, y) || anotherDefinition(x, y))
-      case _ => ???
-    }
-  }
-
-  def disjunct(another: Relation[T, A, B]): Relation[T, A, B] = {
-    another match {
-      case DefinableRelation(anotherDom, anotherCodom, anotherDefinition) =>
-        new DefinableRelation(dom, codom, (x, y) => this.definition(x, y) && anotherDefinition(x, y))
+      case DefinableRelation( anotherDom, anotherCodom, anotherDefinition ) =>
+        new DefinableRelation( dom, codom, ( x, y ) => this.definition( x, y ) || anotherDefinition( x, y ) )
       case _ => ???
     }
   }
 
-  def complement(): Relation[T, A, B] = {
-    new DefinableRelation(dom, codom, (x, y) => !this.definition(x, y))
-  }
-
-  def inverse(): Relation[T, B, A] = {
-    new DefinableRelation(codom, dom, (x, y) => this.definition(y, x))
-  }
-
-  def composite[C <: Set[T]](another: Relation[T, B, C]): Relation[T, A, C] = {
+  def disjunct( another : Relation[T] ) : Relation[T] = {
     another match {
-      case DefinableRelation(anotherDom, anotherCodom, anotherDefinition) =>
-        new DefinableRelation(dom, anotherCodom, (u, w) => codom.forsome(v => this.definition(u, v) && anotherDefinition(v, w)))
+      case DefinableRelation( anotherDom, anotherCodom, anotherDefinition ) =>
+        new DefinableRelation( dom, codom, ( x, y ) => this.definition( x, y ) && anotherDefinition( x, y ) )
       case _ => ???
     }
   }
-}
 
-object Relation {
-  val empty = ???
-  val complete = ???
-  val id = ???
+  def complement() : Relation[T] = {
+    new DefinableRelation( dom, codom, ( x, y ) => !this.definition( x, y ) )
+  }
+
+  def inverse() : Relation[T] = {
+    new DefinableRelation( codom, dom, ( x, y ) => this.definition( y, x ) )
+  }
+
+  def composite[C <: Set[T]]( another : Relation[T] ) : Relation[T] = {
+    another match {
+      case DefinableRelation( anotherDom, anotherCodom, anotherDefinition ) =>
+        new DefinableRelation( dom, anotherCodom, ( u, w ) => codom.forsome( v => this.definition( u, v ) && anotherDefinition( v, w ) ) )
+      case _ => ???
+    }
+  }
+
+  def empty() : Relation[T] = {
+    new DefinableRelation( dom, codom, ( x, y ) => false )
+  }
+
+  def complete() : Relation[T] = {
+    new DefinableRelation( dom, codom, ( x, y ) => true )
+  }
+
+  def id() : Relation[T] = {
+    if ( !dom.empty().includes( dom.disjoint( codom ) ) )
+      new DefinableRelation( dom, codom, ( x, y ) => x == y )
+    else
+      ???
+  }
+
+  def lid() : Relation[T] = {
+    new DefinableRelation( dom, dom, ( x, y ) => x == y )
+  }
+
+  def rid() : Relation[T] = {
+    new DefinableRelation( codom, codom, ( x, y ) => x == y )
+  }
+
 }
--- src/main/scala/math/Set.scala
+++ src/main/scala/math/Set.scala
@@ -3,74 +3,76 @@ package math
 import logic.Forall
 import logic.Forsome
 
-/**
- * Set
- *
- * A set under some representation in T, and also T is treated as the
- * universal set
- *
- */
+/** Set
+  *
+  * A set under some representation in T, and also T is treated as the
+  * universal set
+  *
+  */
 
 trait Set[T] {
-  
-  val forall: Forall[T] = ???
-  val forsome: Forsome[T] = ???
 
-  def contains(entry: T): Boolean
-  def includes(another: Set[T]): Boolean
+  val forall : Forall[T] = ???
+  val forsome : Forsome[T] = ???
 
-  def union(another: Set[T]): Set[T]
-  def intersect(another: Set[T]): Set[T]
-  def disjoint(another: Set[T]): Set[T]
+  def empty() : Set[T]
+  def complete() : Set[T]
 
-  def complement(): Set[T]
+  def contains( entry : T ) : Boolean
+  def includes( another : Set[T] ) : Boolean
 
-}
+  def union( another : Set[T] ) : Set[T]
+  def intersect( another : Set[T] ) : Set[T]
+  def disjoint( another : Set[T] ) : Set[T]
 
-case class DefinableSet[T](val definition: T => Boolean) extends Set[T] {
-  
-  def contains(entry: T): Boolean = definition(entry)
-  def includes(another: Set[T]): Boolean = {
-    another.forall((anotherElem) => definition(anotherElem))
-  }
-
-  def union(another: Set[T]): Set[T] = {
-    another match {
-      case DefinableSet(anotherDefinition) => new DefinableSet({ x => this.definition(x) || anotherDefinition(x) })
-      case _ => ???
-    }
-  }
-
-  def intersect(another: Set[T]): Set[T] = {
-    another match {
-      case DefinableSet(anotherDefinition) => new DefinableSet({ x => this.definition(x) && anotherDefinition(x) })
-      case _ => ???
-    }
-  }
-
-  def disjoint(another: Set[T]): Set[T] = {
-    another match {
-      case DefinableSet(anotherDefinition) => new DefinableSet({ x => this.definition(x) && !anotherDefinition(x) })
-      case _ => ???
-    }
-  }
-
-  def complement(): Set[T] = new DefinableSet({ x => !this.definition(x) })
+  def complement() : Set[T]
 
 }
 
-class Empty[T] extends DefinableSet[T]({ _ => false }) {
-  object Qualification {
-    val forall : Forall[T] = new Forall[T] {
-      def apply(predicate: T => Boolean): Boolean = true
-    }
-    val forsome : Forsome[T] = new Forsome[T] {
-      def apply(predicate: T => Boolean): Boolean = false
+case class DefinableSet[T]( val definition : T => Boolean ) extends Set[T] {
+
+  def contains( entry : T ) : Boolean = definition( entry )
+
+  def includes( another : Set[T] ) : Boolean = {
+    another.forall( ( anotherElem ) => definition( anotherElem ) )
+  }
+
+  def union( another : Set[T] ) : Set[T] = {
+    another match {
+      case DefinableSet( anotherDefinition ) => new DefinableSet( { x => this.definition( x ) || anotherDefinition( x ) } )
+      case _                                 => ???
     }
   }
-}
 
-object Set {
-  val emptySet: Empty[Any] = new Empty[Any]()
+  def intersect( another : Set[T] ) : Set[T] = {
+    another match {
+      case DefinableSet( anotherDefinition ) => new DefinableSet( { x => this.definition( x ) && anotherDefinition( x ) } )
+      case _                                 => ???
+    }
+  }
+
+  def disjoint( another : Set[T] ) : Set[T] = {
+    another match {
+      case DefinableSet( anotherDefinition ) => new DefinableSet( { x => this.definition( x ) && !anotherDefinition( x ) } )
+      case _                                 => ???
+    }
+  }
+
+  def complement() : Set[T] = new DefinableSet( { x => !this.definition( x ) } )
+
+  class Empty extends DefinableSet[T]( { _ => false } ) {
+    override val forall : Forall[T] = new Forall[T] {
+      def apply( predicate : T => Boolean ) : Boolean = true
+    }
+    override val forsome : Forsome[T] = new Forsome[T] {
+      def apply( predicate : T => Boolean ) : Boolean = false
+    }
+  }
+
+  class Universal extends DefinableSet[T]( { _ => true } )
+
+  def empty() : Set[T] = new Empty
+  def complete() : Set[T] = new Universal
+
 }
 
